efafd50d2fb2ce8135d0ce6b15390ed82cc27f67
Dylan K <dkooby3@gmail.com>
2023-12-04 14:27:31 -0500

    Display Puppy Names

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
 import './App.css'
+import {puppyList} from './data.js'
+import { useState } from 'react'
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies)
   return (
     <div>
-
+      {
+        puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>
+        })
+      }
     </div>
   )
 }
